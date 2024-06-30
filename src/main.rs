c4e6a7560549352eff2a9ea287afb75f2d4d11ac
Bhuwan Pandit <bp7968h@gre.ac.uk>
2024-06-30 23:31:59 +0100

    Send Response according to Request-URI

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::{prelude::*, BufReader};
-
+use std::fs;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap_or_else( |e| {
@@ -16,16 +16,27 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream){
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let length = contents.len();
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let length = contents.len();
 
-    stream.write_all(response.as_bytes()).unwrap();
+        let response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+        );
+
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("other.html").unwrap();
+        let length = contents.len();
+
+        let response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+        );
+
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 }
