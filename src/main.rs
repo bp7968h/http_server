f1fd9e1ea32116ad728bb0c74318c4392394b7c3
Bhuwan Pandit <bp7968h@gre.ac.uk>
2024-06-30 23:22:02 +0100

    Add Single Threaded Http Request and Response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::net::{TcpListener, TcpStream};
+use std::io::{prelude::*, BufReader};
+
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap_or_else( |e| {
+        panic!("Binding Failed: {}",e);
+    });
+
+    for stream in listener.incoming(){
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream){
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write_all(response.as_bytes()).unwrap();
 }
